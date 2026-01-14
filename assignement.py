433b25d183addd2509d0f1d61a4fcdfb196acce2
nwubumagrace-design <nwubumagrace@gmail.com>
2026-01-14 14:16:07 +0100

    Update assignement.py

--- assignement.py
+++ assignement.py
@@ -92,5 +92,27 @@ elif Choice == 2 :
 else :
 print ('Language not found')
 
-
-
+word = input ( 'enter your english word:')
+print(d[word])
+def hausa_dict():
+words = {
+    "chair": "kujera"
+    "stone": "dutse"
+    "bag": "jaka"
+    "shoe": "takalmi"
+    "wrapper": "zani"
+    "phone": "waya"
+    "bed": "godo"
+    "food": "abichin"
+    "clothes": "kaya"
+    "plates": "kwanu"
+    "bottle": "gora" 
+    "scissors": "almakachi"
+    "knife": "wuka"
+    "basket": "kwondo"
+    "house": "Gida"
+    "money": "kudi" 
+    "car": "mota"
+    "water": "ruwa"
+    "horse": "doki"
+    "bone': "Kashi"
